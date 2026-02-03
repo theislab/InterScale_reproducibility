b565a87df02e1b4c9697e37ce95c2a9056c57a16
Francesca Drummer <francesca.drummer@googlemail.com>
2026-02-03 09:44:59 +0100

    node class

---
+++ scripts/melton_jimenez_25/node_class.py
@@ -0,0 +1,10 @@
+# %% [markdown]
+# # Node classification sweep performance 
+#
+# Plots showing the performance for the graph classification on the CosMx Pancreas data by Melton et. al, 2025.
+#
+# - InterScale (GCN + Transformer)
+# - GCN
+# - PCATransformer
+
+# %%
